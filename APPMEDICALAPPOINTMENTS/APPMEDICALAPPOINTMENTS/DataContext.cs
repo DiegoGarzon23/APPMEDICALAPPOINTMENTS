0ebcd63080a54099f8232777867d321158e2b63e
DiegoGarzon23 <garzoncdiego@hotmail.com>
2020-04-16 18:28:30 -0500

    DataContext

---
+++ APPMEDICALAPPOINTMENTS/APPMEDICALAPPOINTMENTS/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using APPMEDICALAPPOINTMENTS.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace APPMEDICALAPPOINTMENTS
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Medic> Medics { get; set; }
+        public DbSet<Pacient> Pacients { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
