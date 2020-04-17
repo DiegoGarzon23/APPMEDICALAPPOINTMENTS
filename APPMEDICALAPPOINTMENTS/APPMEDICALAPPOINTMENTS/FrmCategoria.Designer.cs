6851aa5e538edb3abb66a6d89fdc4a09f6942ff1
DiegoGarzon23 <garzoncdiego@hotmail.com>
2020-04-16 22:08:47 -0500

    Forma Categoria

---
+++ APPMEDICALAPPOINTMENTS/APPMEDICALAPPOINTMENTS/FrmCategoria.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace APPMEDICALAPPOINTMENTS
+{
+    partial class FrmCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grdDatos = new MetroFramework.Controls.MetroGrid();
+            this.pnlDatos = new MetroFramework.Controls.MetroPanel();
+            this.txtCategoryName = new MetroFramework.Controls.MetroTextBox();
+            this.lblCategoryName = new MetroFramework.Controls.MetroLabel();
+            this.txtId = new MetroFramework.Controls.MetroTextBox();
+            this.lblId = new MetroFramework.Controls.MetroLabel();
+            this.btnGuardar = new MetroFramework.Controls.MetroButton();
+            this.btnEditar = new MetroFramework.Controls.MetroButton();
+            this.btnAgregar = new MetroFramework.Controls.MetroButton();
+            this.btnSalir = new MetroFramework.Controls.MetroButton();
+            this.btnEliminar = new MetroFramework.Controls.MetroButton();
+            this.categoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.medicsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).BeginInit();
+            this.pnlDatos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdDatos
+            // 
+            this.grdDatos.AllowUserToResizeRows = false;
+            this.grdDatos.AutoGenerateColumns = false;
+            this.grdDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.grdDatos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grdDatos.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.grdDatos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdDatos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grdDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.categoryNameDataGridViewTextBoxColumn,
+            this.medicsDataGridViewTextBoxColumn});
+            this.grdDatos.DataSource = this.categoryBindingSource1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grdDatos.DefaultCellStyle = dataGridViewCellStyle2;
+            this.grdDatos.EnableHeadersVisualStyles = false;
+            this.grdDatos.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.grdDatos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.grdDatos.Location = new System.Drawing.Point(23, 63);
+            this.grdDatos.Name = "grdDatos";
+            this.grdDatos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grdDatos.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.grdDatos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grdDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdDatos.Size = new System.Drawing.Size(248, 150);
+            this.grdDatos.TabIndex = 0;
+            // 
+            // pnlDatos
+            // 
+            this.pnlDatos.Controls.Add(this.txtCategoryName);
+            this.pnlDatos.Controls.Add(this.lblCategoryName);
+            this.pnlDatos.Controls.Add(this.txtId);
+            this.pnlDatos.Controls.Add(this.lblId);
+            this.pnlDatos.HorizontalScrollbarBarColor = true;
+            this.pnlDatos.HorizontalScrollbarHighlightOnWheel = false;
+            this.pnlDatos.HorizontalScrollbarSize = 10;
+            this.pnlDatos.Location = new System.Drawing.Point(277, 63);
+            this.pnlDatos.Name = "pnlDatos";
+            this.pnlDatos.Size = new System.Drawing.Size(579, 150);
+            this.pnlDatos.TabIndex = 1;
+            this.pnlDatos.VerticalScrollbarBarColor = true;
+            this.pnlDatos.VerticalScrollbarHighlightOnWheel = false;
+            this.pnlDatos.VerticalScrollbarSize = 10;
+            // 
+            // txtCategoryName
+            // 
+            // 
+            // 
+            // 
+            this.txtCategoryName.CustomButton.Image = null;
+            this.txtCategoryName.CustomButton.Location = new System.Drawing.Point(405, 1);
+            this.txtCategoryName.CustomButton.Name = "";
+            this.txtCategoryName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtCategoryName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtCategoryName.CustomButton.TabIndex = 1;
+            this.txtCategoryName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtCategoryName.CustomButton.UseSelectable = true;
+            this.txtCategoryName.CustomButton.Visible = false;
+            this.txtCategoryName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.categoryBindingSource, "CategoryName", true));
+            this.txtCategoryName.Lines = new string[0];
+            this.txtCategoryName.Location = new System.Drawing.Point(138, 53);
+            this.txtCategoryName.MaxLength = 32767;
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.PasswordChar = '\0';
+            this.txtCategoryName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtCategoryName.SelectedText = "";
+            this.txtCategoryName.SelectionLength = 0;
+            this.txtCategoryName.SelectionStart = 0;
+            this.txtCategoryName.ShortcutsEnabled = true;
+            this.txtCategoryName.Size = new System.Drawing.Size(427, 23);
+            this.txtCategoryName.TabIndex = 5;
+            this.txtCategoryName.UseSelectable = true;
+            this.txtCategoryName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtCategoryName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblCategoryName
+            // 
+            this.lblCategoryName.AutoSize = true;
+            this.lblCategoryName.Location = new System.Drawing.Point(11, 55);
+            this.lblCategoryName.Name = "lblCategoryName";
+            this.lblCategoryName.Size = new System.Drawing.Size(121, 19);
+            this.lblCategoryName.TabIndex = 4;
+            this.lblCategoryName.Text = "Nombre Categoria";
+            // 
+            // txtId
+            // 
+            // 
+            // 
+            // 
+            this.txtId.CustomButton.Image = null;
+            this.txtId.CustomButton.Location = new System.Drawing.Point(115, 1);
+            this.txtId.CustomButton.Name = "";
+            this.txtId.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtId.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtId.CustomButton.TabIndex = 1;
+            this.txtId.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtId.CustomButton.UseSelectable = true;
+            this.txtId.CustomButton.Visible = false;
+            this.txtId.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.categoryBindingSource, "Id", true));
+            this.txtId.Lines = new string[0];
+            this.txtId.Location = new System.Drawing.Point(138, 24);
+            this.txtId.MaxLength = 32767;
+            this.txtId.Name = "txtId";
+            this.txtId.PasswordChar = '\0';
+            this.txtId.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtId.SelectedText = "";
+            this.txtId.SelectionLength = 0;
+            this.txtId.SelectionStart = 0;
+            this.txtId.ShortcutsEnabled = true;
+            this.txtId.Size = new System.Drawing.Size(137, 23);
+            this.txtId.TabIndex = 3;
+            this.txtId.UseSelectable = true;
+            this.txtId.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtId.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(11, 24);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(20, 19);
+            this.lblId.TabIndex = 2;
+            this.lblId.Text = "Id";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(277, 228);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(111, 38);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseSelectable = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(394, 228);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(111, 38);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseSelectable = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(511, 228);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(111, 38);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseSelectable = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(745, 228);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(111, 38);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseSelectable = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(628, 228);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(111, 38);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseSelectable = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // categoryBindingSource
+            // 
+            this.categoryBindingSource.DataSource = typeof(APPMEDICALAPPOINTMENTS.Data.Category);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // categoryNameDataGridViewTextBoxColumn
+            // 
+            this.categoryNameDataGridViewTextBoxColumn.DataPropertyName = "CategoryName";
+            this.categoryNameDataGridViewTextBoxColumn.HeaderText = "CategoryName";
+            this.categoryNameDataGridViewTextBoxColumn.Name = "categoryNameDataGridViewTextBoxColumn";
+            // 
+            // medicsDataGridViewTextBoxColumn
+            // 
+            this.medicsDataGridViewTextBoxColumn.DataPropertyName = "Medics";
+            this.medicsDataGridViewTextBoxColumn.HeaderText = "Medics";
+            this.medicsDataGridViewTextBoxColumn.Name = "medicsDataGridViewTextBoxColumn";
+            // 
+            // categoryBindingSource1
+            // 
+            this.categoryBindingSource1.DataSource = typeof(APPMEDICALAPPOINTMENTS.Data.Category);
+            // 
+            // FrmCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 335);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pnlDatos);
+            this.Controls.Add(this.grdDatos);
+            this.Name = "FrmCategoria";
+            this.Text = "Categoria";
+            this.Load += new System.EventHandler(this.FrmCategoria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDatos)).EndInit();
+            this.pnlDatos.ResumeLayout(false);
+            this.pnlDatos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid grdDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn medicsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource categoryBindingSource;
+        private MetroFramework.Controls.MetroPanel pnlDatos;
+        private MetroFramework.Controls.MetroTextBox txtCategoryName;
+        private MetroFramework.Controls.MetroLabel lblCategoryName;
+        private MetroFramework.Controls.MetroTextBox txtId;
+        private MetroFramework.Controls.MetroLabel lblId;
+        private MetroFramework.Controls.MetroButton btnGuardar;
+        private MetroFramework.Controls.MetroButton btnEditar;
+        private MetroFramework.Controls.MetroButton btnAgregar;
+        private MetroFramework.Controls.MetroButton btnSalir;
+        private MetroFramework.Controls.MetroButton btnEliminar;
+        private System.Windows.Forms.BindingSource categoryBindingSource1;
+    }
+}
---
+++ APPMEDICALAPPOINTMENTS/APPMEDICALAPPOINTMENTS/FrmCategoria.cs
@@ -0,0 +1,107 @@
+﻿using APPMEDICALAPPOINTMENTS.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APPMEDICALAPPOINTMENTS
+{
+    public partial class FrmCategoria : MetroFramework.Forms.MetroForm
+    {
+        private object userBindingSource;
+
+        public FrmCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCategoria_Load(object sender, EventArgs e)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                categoryBindingSource.DataSource =
+                    dataContext.Categories.ToList();
+            }
+            pnlDatos.Enabled = false;
+            Category category = categoryBindingSource.Current as Category;
+                
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (DataContext dataContext = new DataContext())
+            {
+                Category category =
+                    categoryBindingSource.Current as Category;
+                if (category != null)
+                {
+                    if (dataContext.Entry<Category>(category).State == EntityState.Detached)
+                        dataContext.Set<Category>().Attach(category);
+                    if (category.Id == 0)
+                        dataContext.Entry<Category>(category).State = EntityState.Added;
+                    else
+                        dataContext.Entry<Category>(category).State = EntityState.Modified;
+                    dataContext.SaveChanges();
+                    MetroFramework.MetroMessageBox.Show(this, "Categoria guardada");
+                    grdDatos.Refresh();
+                    pnlDatos.Enabled = false;
+                }
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            pnlDatos.Enabled = true;
+            txtCategoryName.Focus();
+            Category category =
+                categoryBindingSource.Current as Category;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            pnlDatos.Enabled = true;
+            categoryBindingSource.Add(new Category());
+            categoryBindingSource.MoveLast();
+            txtCategoryName.Focus();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (MetroFramework.MetroMessageBox.Show(this,
+               "¿Quieres eliminar el registro?",
+               "Eliminar",
+               MessageBoxButtons.OKCancel,
+               MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                using (DataContext dataContext = new DataContext())
+                {
+                    Category category =
+                        categoryBindingSource.Current as Category;
+                    if (category != null)
+                    {
+                        if (dataContext.Entry<Category>(category).State == EntityState.Detached)
+                            dataContext.Set<Category>().Attach(category);
+                        dataContext.Entry<Category>(category).State = EntityState.Deleted;
+                        dataContext.SaveChanges();
+                        MetroFramework.MetroMessageBox.Show(this, "Empleado eliminado");
+                        categoryBindingSource.RemoveCurrent();
+                        pnlDatos.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            pnlDatos.Enabled = false;
+            categoryBindingSource.ResetBindings(false);
+            FrmCategoria_Load(sender, e);
+        }
+    }
+}
